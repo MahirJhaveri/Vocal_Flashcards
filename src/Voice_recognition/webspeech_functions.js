26671b329ed10a4624523def7bc67074164ceb81
Christian Roncal <cjl.roncal@gmail.com>
2018-04-07 19:48:09 -0400

    created exportable functions for voice recognition (1)

---
+++ src/Voice_recognition/webspeech_functions.js
@@ -0,0 +1,7 @@
+
+exports.randomPhrase() = function randomPhrase() {
+    //TODO CHANGE BACK TO JP
+  var number = Math.floor(Math.random() * answers.length);
+  var number = Math.floor(Math.random() * questions.length);
+  return number;
+}
